c62e8d9ef5f91eefacec3b1c6298fccde000fc7c
Kamronbek Rakhimov <156855800+rkamron@users.noreply.github.com>
2025-01-24 22:03:16 -0500

    tabs done

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -8,6 +8,8 @@ import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+import AntDesign from '@expo/vector-icons/AntDesign';
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
@@ -29,15 +31,23 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'HOME',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
+      
       <Tabs.Screen
-        name="explore"
+        name="camera"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'NEW',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="leaf.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'SETTINGS',
+          tabBarIcon: ({ color }) => <AntDesign name="setting" size={24} color={color} />,
         }}
       />
     </Tabs>
