bc1990b18a707fb03a52f397f567876881ce270b
Kent Dang <kenttdang@gmail.com>
2025-01-25 11:29:23 -0500

    implemented flask

--- server/main.py
+++ server/main.py
@@ -1,12 +1,22 @@
 import sys
 sys.dont_write_bytecode = True
 from model import response
+from flask import Flask, request
+from flask_cors import CORS
 
-def main():
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route('/api/plagent', methods=['GET', 'POST'])
+def plagent():
+   
+   plant_name = request.json.get('plant_name')
+   plant_disease = request.json.get('plant_disease')
+
    workflow = response()
    initial_state = {
-       "plant_name": "Rose",
-       "plant_disease": "Powdery Mildew",
+       "plant_name": plant_name,
+       "plant_disease": plant_disease,
        "loading_state": "Identification Process",
        "content": [],
        "result": "",
@@ -17,6 +27,7 @@ def main():
    for state in workflow.graph.stream(initial_state):
        final_state = state
    print(final_state['generate']['result'])
+   return final_state(['generate']['result'])
 
 if __name__ == "__main__":
-   main()
+   app.run(debug=True, port=8080)
