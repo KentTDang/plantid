14119b049393841735eaa1da163451fa50f54a2a
Kent Dang <kenttdang@gmail.com>
2025-01-25 11:28:34 -0500

    auth rework for tabs

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,49 +1,56 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
-import "react-native-reanimated";
-import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
-import { Slot } from "expo-router";
-import { tokenCache } from "../cache";
-import { useColorScheme } from "@/hooks/useColorScheme";
+import { useEffect } from 'react';
+import * as SecureStore from 'expo-secure-store';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+import { Slot, useRouter, useSegments } from 'expo-router';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
+// Cache the Clerk JWT
+const tokenCache = {
+	async getToken(key: string) {
+		try {
+			return SecureStore.getItemAsync(key);
+		} catch (err) {
+			return null;
+		}
+	},
+	async saveToken(key: string, value: string) {
+		try {
+			return SecureStore.setItemAsync(key, value);
+		} catch (err) {
+			return;
+		}
+	}
+};
 
-  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+const InitialLayout = () => {
+	const { isLoaded, isSignedIn } = useAuth();
+	const segments = useSegments();
+	const router = useRouter();
 
-  if (!publishableKey) {
-    throw new Error("Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY to your .env file");
-  }
+	// If the user is signed in, redirect them to the home page
+	// If the user is not signed in, redirect them to the login page
+	useEffect(() => {
+		if (!isLoaded) return;
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+		const inTabsGroup = segments[0] === '(auth)';
 
-  if (!loaded) {
-    return null;
-  }
+		if (isSignedIn && !inTabsGroup) {
+			router.replace('/home');
+		} else if (!isSignedIn) {
+			router.replace('/signIn');
+		}
+	}, [isSignedIn]);
 
-  return (
-    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <ClerkLoaded>
-        <Slot />
-      </ClerkLoaded>
-    </ClerkProvider>
-  );
-}
+	return <Slot />;
+};
+
+const RootLayoutNav = () => {
+	return (
+		<ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache}>
+			<InitialLayout />
+		</ClerkProvider>
+	);
+};
+
+export default RootLayoutNav;
