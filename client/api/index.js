468ffd75c61cf05a777a1bd40b2480788cd529e1
Kent Dang <kenttdang@gmail.com>
2025-01-25 21:28:49 -0500

    file upload

--- client/api/index.js
+++ client/api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 const port = 3000;
